a92316c4424f263ded324f577488201c0e08dc98
karcagtamas <karcagtamas@gmail.com>
2018-11-15 10:24:52 +0100

    If_Then_Else_ElseIf_Case

---
+++ Case_Then.sql
@@ -0,0 +1,16 @@
+﻿USE teszt;
+
+CREATE PROCEDURE Nap(IN d varchar(20))
+  BEGIN
+  CASE DAYNAME(d)
+    WHEN 'Monday' THEN SELECT d, 'Hétfő';
+    WHEN 'Tuesday' THEN SELECT d, 'Kedd';
+    WHEN 'Wednesday' THEN SELECT d, 'Szerda';
+    WHEN 'Thursday' THEN SELECT d, 'Csütörtök';
+    WHEN 'Friday' THEN SELECT d, 'Péntek';
+    WHEN 'Saturday' THEN SELECT d, 'Szombat';
+    WHEN 'Sunday' THEN SELECT d, 'Vasárnap';
+    END CASE;
+  END;
+
+  CALL Nap('1999-07-26');
---
+++ If_Then_IfElse_Else.sql
@@ -0,0 +1,16 @@
+﻿
+USE test;
+
+CREATE PROCEDURE Nap(IN d varchar(20))
+  BEGIN
+  IF DAYNAME(d) = 'Monday' THEN SELECT d, 'Hétfő';
+    ELSEIF DAYNAME(d) = 'Tuesday' THEN SELECT d, 'Kedd';
+    ELSEIF DAYNAME(d) = 'Wednesday' THEN SELECT d, 'Szerda';
+    ELSEIF DAYNAME(d) = 'Thursday' THEN SELECT d, 'Csütörtök';
+  ELSEIF DAYNAME(d) = 'Friday' THEN SELECT d, 'Péntek';
+  ELSEIF DAYNAME(d) = 'Saturday' THEN SELECT d, 'Szombat';
+  ELSE SELECT d, 'Vasárnap';
+  END IF;
+  END;
+
+  CALL Nap('1999-07-26');
